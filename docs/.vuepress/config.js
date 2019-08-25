11a1997c8611560bf5ac262948ec035ba3f59ba5
Takuya KIHARA <gray@tacck.net>
2019-08-25 12:44:07 +0900

    :lipstick: Change 'Lastupdated' time format.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 module.exports = {
     title: 'Meetup.Tacck.NET',
     description: 'meetup.tacck.net',
@@ -14,5 +16,18 @@ module.exports = {
             ['https://www.tacck.net', 'Tacck.NET'],
             ['https://blog.tacck.net', 'Tech Blog']
         ]
-    }
+    },
+    plugins: [
+        [
+            '@vuepress/last-updated',
+            {
+                transformer: (timestamp, lang) => {
+                    // Don't forget to install moment yourself
+                    const moment = require('moment')
+                    moment.locale(lang)
+                    return moment(timestamp).format('YYYY-MM-DD HH:mm:SS')
+                }
+            }
+        ]
+    ]
 }
